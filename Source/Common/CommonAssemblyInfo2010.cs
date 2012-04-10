380a2ebbe4053139d616795723afa8ddbb0e9c5d
mikeFourie_cp <SND\mikeFourie_cp>
2012-04-10 14:56:46 -0700

    Adding 2010 reference folder and assembly info for 2010 to Main....

---
+++ Source/Common/CommonAssemblyInfo2010.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CommonAssemblyInfo.cs">(c) http://TfsBuildExtensions.codeplex.com/. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+//-----------------------------------------------------------------------
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.4.1")]
+[assembly: AssemblyInformationalVersion("1.10.4.1")]
+[assembly: AssemblyCompany("Community TFS Build Extensions http://TfsBuildExtensions.codeplex.com/")]
+[assembly: AssemblyCopyright("Copyright ©s Community TFS Build Extensions http://TfsBuildExtensions.codeplex.com/")]
+[assembly: AssemblyTrademark("Community TFS Build Extensions http://TfsBuildExtensions.codeplex.com/")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("Community TFS Build Extensions")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
