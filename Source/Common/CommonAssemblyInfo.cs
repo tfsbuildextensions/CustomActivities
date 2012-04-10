e30291a106254915522625071e76f7e850360214
mikeFourie_cp <SND\mikeFourie_cp>
2012-04-10 07:56:42 -0700

    merge revision update

--- Source/Common/CommonAssemblyInfo.cs
+++ Source/Common/CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // <copyright file="CommonAssemblyInfo.cs">(c) http://TfsBuildExtensions.codeplex.com/. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
 //-----------------------------------------------------------------------
 using System.Reflection;
@@ -6,10 +6,10 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.11.0.0")]
-[assembly: AssemblyFileVersion("1.11.4.0")]
-[assembly: AssemblyInformationalVersion("1.11.4.0")]
+[assembly: AssemblyFileVersion("1.11.4.1")]
+[assembly: AssemblyInformationalVersion("1.11.4.1")]
 [assembly: AssemblyCompany("Community TFS Build Extensions http://TfsBuildExtensions.codeplex.com/")]
-[assembly: AssemblyCopyright("Copyright ©s Community TFS Build Extensions http://TfsBuildExtensions.codeplex.com/")]
+[assembly: AssemblyCopyright("Copyright �s Community TFS Build Extensions http://TfsBuildExtensions.codeplex.com/")]
 [assembly: AssemblyTrademark("Community TFS Build Extensions http://TfsBuildExtensions.codeplex.com/")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyCulture("")]
