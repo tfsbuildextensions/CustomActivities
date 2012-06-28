3026157b6b140926e33b2a56b140157e3fd8a99a
OsirisJakob_cp <SND\OsirisJakob_cp>
2012-06-28 15:48:16 -0700

    Adding template solution that can be used as a starter when creating custom activities and/or working with custom build process templates

---
+++ Templates/CustomProcesses/CustomActivities/HelloWorldActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Microsoft.TeamFoundation.Build.Client;
+
+namespace CustomActivities
+{
+    [BuildActivity(HostEnvironmentOption.All)]
+    public sealed class HelloWorldActivity : CodeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<string> Text { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtain the runtime value of the Text input argument
+            string text = context.GetValue(this.Text);
+        }
+    }
+}
