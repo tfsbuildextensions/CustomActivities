d64b5f5cd71a3a9bf2995c4ba9c234670afc1c1f
Miiitch_cp <SND\Miiitch_cp>
2012-12-14 14:19:50 -0800

    Old NAnt activity folder removed

--- Source/Activities.NAnt/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs">(c) http://TfsBuildExtensions.codeplex.com/. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
-//---------------------------------------------------------------------
-using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Community TFS Build Extensions for NAnt")]
-[assembly: AssemblyDescription("Community TFS Build Extensions for NAnt")]
-[assembly: CLSCompliant(false)]
