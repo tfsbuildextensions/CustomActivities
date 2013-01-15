9d31d80dee89ba7c510c8917bc3b452c6a967d54
mikeFourie_cp <SND\mikeFourie_cp>
2013-01-15 15:11:39 -0800

    Adding Sleep activity

---
+++ Source/Activities/Framework/Sleep.cs
@@ -0,0 +1,29 @@
+//-----------------------------------------------------------------------
+// <copyright file="Sleep.cs">(c) http://TfsBuildExtensions.codeplex.com/. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+//-----------------------------------------------------------------------
+namespace TfsBuildExtensions.Activities.Framework
+{
+    using System.Activities;
+    using System.Threading;
+    using Microsoft.TeamFoundation.Build.Client;
+
+    /// <summary>
+    /// Sleep a thread for the specified number of milliseconds
+    /// </summary>
+    [BuildActivity(HostEnvironmentOption.Agent)]
+    public sealed class Sleep : BaseCodeActivity
+    {
+        /// <summary>
+        /// Sepecifies the number of milliseconds to sleep for
+        /// </summary>
+        [RequiredArgument]
+        public InArgument<int> NumberOfMilliseconds { get; set; }
+
+        protected override void InternalExecute()
+        {
+            int numberOfMillisecs = this.NumberOfMilliseconds.Get(this.ActivityContext);
+            this.LogBuildMessage(string.Format("Sleeping for {0} milliseconds", numberOfMillisecs));
+            Thread.Sleep(numberOfMillisecs);
+        }
+    }
+}
