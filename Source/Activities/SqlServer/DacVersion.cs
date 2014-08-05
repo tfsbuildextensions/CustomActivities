a13dd2b9a43cdda5f65d3d61304b160e3832b6c5
KjartanThor <kjartan@outlook.com>
2014-08-05 18:07:36 +0000

    Changed to enable multiple project files
    Updated the DacVersion activity so that it has a Files property like the
    TfsVersion activity so that it's possible to have more than one file
    versioned at the same time. I left the current property intact so as not
    to break working builds. This new property also enables using the
    FindMatchingFiles activity to find the files to act upon and so create a
    more dynamic build workflow.
    
    Also changed how the activity finds the DacVersion tag to use
    XmlDocument class instead of naive string search that does not account
    for any version of the tag other than opening and closing tags being on
    the same line.
    
    Added a new unit test to test the new functionality and fixed the old
    test so that it does not rely on an external file that is not a part of
    the solution in a specific place to work, so now it works the same as
    the TfsVersion activity tests.

--- Source/Activities/SqlServer/DacVersion.cs
+++ Source/Activities/SqlServer/DacVersion.cs
@@ -3,17 +3,24 @@
 //-----------------------------------------------------------------------
 namespace TfsBuildExtensions.Activities.SqlServer
 {
-    using System;
     using System.Activities;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
+    using System.Xml;
     using Microsoft.TeamFoundation.Build.Client;
 
     /// <summary>
     /// Updates the DacVersion version number in any .sqlproj file inside a solution.
     /// </summary>
     [BuildActivity(HostEnvironmentOption.All)]
-    public sealed class DacVersion : BaseCodeActivity<string>
+    public sealed class DacVersion : BaseCodeActivity
     {
+        /// <summary>
+        /// Gets or sets the files where the DacVersion tag is to be updated.
+        /// </summary>
+        public InArgument<IEnumerable<string>> Files { get; set; }
+
         /// <summary>
         /// The absolute path to the .sqlproj file in solution
         /// </summary>
@@ -28,46 +35,64 @@ namespace TfsBuildExtensions.Activities.SqlServer
         /// InternalExecute
         /// </summary>
         /// <returns>string</returns>
-        protected override string InternalExecute()
+        protected override void InternalExecute()
         {
-            string path = this.SqlProjFilePath.Get(this.ActivityContext);
-            string fileStr = string.Empty;
-            foreach (var line in File.ReadLines(path))
+            if (string.IsNullOrWhiteSpace(this.ActivityContext.GetValue(this.Version)))
             {
-                if (line.Trim().Contains("<DacVersion>"))
-                {
-                    string currLine = line.Trim();
-                    int i = currLine.IndexOf("<DacVersion>", StringComparison.OrdinalIgnoreCase);
-                    int j = currLine.IndexOf("</DacVersion>", StringComparison.OrdinalIgnoreCase) + "</DacVersion>".Length;
-                    string currDacVersionElement = currLine.Substring(i, j);
-                    fileStr += currLine.Replace(currDacVersionElement, "<DacVersion>" + this.Version.Get(this.ActivityContext) + "</DacVersion>");
-                }
-                else
-                {
-                    fileStr += line;
-                }
+                this.LogBuildError("Version is required.");
+                return;
             }
 
-            // write to file (unset and set back ReadOnly attribute if present).
-            var fileAttributes = File.GetAttributes(path);
-            var attributesChanged = false;
+            IEnumerable<string> filesPaths = this.ActivityContext.GetValue(this.Files);
+            if (null != filesPaths && filesPaths.Any())
+            {
+                foreach (string filePath in filesPaths)
+                {
+                    UpdateDacVersionInFile(filePath);
+                }
+            }
+            else
+            {
+                string path = this.ActivityContext.GetValue(this.SqlProjFilePath);
+                if (string.IsNullOrWhiteSpace(path))
+                {
+                    this.LogBuildError("If a list of file names is not given for Files, SqlProjFilePath is required.");
+                }
+
+                this.UpdateDacVersionInFile(path);
+            }
+        }
+
+        private void UpdateDacVersionInFile(string filePath)
+        {
+            // First make sure the file is writable.
+            FileAttributes fileAttributes = File.GetAttributes(filePath);
+            var changedAttribute = false;
+
+            // If readonly attribute is set, reset it.
             if ((fileAttributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
             {
-                File.SetAttributes(path, fileAttributes ^ FileAttributes.ReadOnly);
-                attributesChanged = true;
+                this.LogBuildMessage("Making file writable", BuildMessageImportance.Low);
+                File.SetAttributes(filePath, fileAttributes ^ FileAttributes.ReadOnly);
+                changedAttribute = true;
             }
 
-            using (var file = new StreamWriter(path))
+            var document = new XmlDocument();
+            document.Load(filePath);
+
+            var versionNodes = document.GetElementsByTagName("DacVersion", "http://schemas.microsoft.com/developer/msbuild/2003");
+            foreach (XmlNode node in versionNodes)
             {
-                file.Write(fileStr);
+                node.InnerText = this.ActivityContext.GetValue(this.Version);
             }
 
-            if (attributesChanged)
+            document.Save(filePath);
+
+            if (changedAttribute)
             {
-                File.SetAttributes(path, FileAttributes.ReadOnly);
+                this.LogBuildMessage("Making file readonly", BuildMessageImportance.Low);
+                File.SetAttributes(filePath, FileAttributes.ReadOnly);
             }
-
-            return File.ReadAllText(path);
         }
     }
 }
